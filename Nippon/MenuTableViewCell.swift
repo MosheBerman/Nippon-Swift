2bf96a9ffbca89c672258e35ce01981b545a98a9
Moshe Berman <moshberm@gmail.com>
2014-10-20 00:40:48 -0400

    Added a custom cell for the menu.

---
+++ Nippon/MenuTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  MenuTableViewCell.swift
+//  Nippon
+//
+//  Created by Moshe on 10/19/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var primaryLabel: UILabel?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        
+        if animated {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.setTapped(selected)
+            })
+        }
+        else
+        {
+            self.setTapped(selected)
+        }
+    }
+    
+    override func setHighlighted(highlighted: Bool, animated: Bool) {
+        if animated {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.setTapped(highlighted)
+            })
+        }
+        else
+        {
+            self.setTapped(highlighted)
+        }
+    }
+    
+    func setTapped(tapped: Bool) {
+        
+        var backgroundColor = UIColor(white: 0.97, alpha: 0.1)
+        var titleColor = UIColor.whiteColor()
+        var subtitleColor = UIColor.lightGrayColor()
+        
+        if tapped {
+            backgroundColor = UIColor(white: 0.885, alpha: 0.6)
+            subtitleColor = titleColor.colorWithAlphaComponent(0.5)
+        }
+        
+        self.backgroundColor = backgroundColor
+
+        self.primaryLabel?.textColor = titleColor
+        
+    }
+}
