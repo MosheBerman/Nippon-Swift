f5af6ab22e632fafefc4a1478d0d8aad2d0154a1
Moshe Berman <moshberm@gmail.com>
2014-10-21 09:57:41 -0400

    Added an extension to NSLayoutConstraint.

---
+++ Nippon/ConvenientConstraints.swift
@@ -0,0 +1,33 @@
+//
+//  ConvenientConstraints.swift
+//  Nippon
+//
+//  Created by Moshe on 10/20/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+extension NSLayoutConstraint {
+    
+    /**
+    *   A convenience initializer to shorten calls to init.
+    *   Assumes a relation of .Equal, a multiplier of 1.0.
+    */
+    
+    convenience init(item view1: AnyObject, attribute attr1: NSLayoutAttribute, toItem view2: AnyObject?, attribute attr2: NSLayoutAttribute, constant c: CGFloat) {
+        
+        self.init(item: view1, attribute: attr1, relatedBy: .Equal, toItem: view2, attribute: attr2, multiplier: 1.0, constant: c);
+    }
+    
+    /**
+    *   A convenience initializer to shorten calls to init.
+    *   Assumes a relation of .Equal, a multiplier of 1.0,
+    *   and a constant of zero.
+    */
+    
+    convenience init(item view1: AnyObject, attribute attr1: NSLayoutAttribute, toItem view2: AnyObject?, attribute attr2: NSLayoutAttribute) {
+    
+        self.init(item: view1, attribute: attr1, relatedBy: .Equal, toItem: view2, attribute: attr2, multiplier: 1.0, constant: 0);
+    }
+}
