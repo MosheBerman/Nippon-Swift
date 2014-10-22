c47db5ce696736d3268d8eeff616dc165e11eb3f
Moshe Berman <moshberm@gmail.com>
2014-10-21 21:44:28 -0400

    Implemented header.

--- Nippon/MenuHeaderView.swift
+++ Nippon/MenuHeaderView.swift
@@ -8,24 +8,37 @@
 
 import UIKit
 
-class MenuHeaderView: UIView {
+class MenuHeaderView : UIView {
     
-    var icon: UIImageView
+    var image: UIImageView
+    var titleLabel: UILabel
     
     override init(frame: CGRect) {
-        icon = UIImageView(image: UIImage(named: "Icon"))
-        icon.setTranslatesAutoresizingMaskIntoConstraints(false)
-        icon.clipsToBounds = true
-        icon.layer.cornerRadius = 10.0/57.0*100.0
-
         
-        super.init(frame: CGRectMake(0, 0, CGRectGetWidth(UIScreen.mainScreen().bounds), 100))
+        image = UIImageView(image: UIImage(named: "SushiBanner").imageWithRenderingMode(.AlwaysTemplate))
+        image.tintColor = UIColor.whiteColor()
+        image.contentMode = UIViewContentMode.ScaleAspectFit
+        
+        titleLabel = UILabel()
+        titleLabel.textAlignment = .Center
+        titleLabel.backgroundColor = UIColor.clearColor()
+        titleLabel.text = "Nippon"
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.font = UIFont(name: "Avenir-Light", size: 36.0)
+        
+        super.init(frame: CGRectMake(0, 0, CGRectGetWidth(UIScreen.mainScreen().bounds), 184))
     }
     
     required init(coder aDecoder: NSCoder) {
-        icon = UIImageView(image: UIImage(named: "Icon"))
-        icon.layer.cornerRadius = 10.0/57.0*100.0
-        icon.clipsToBounds = true
+        
+        image = UIImageView(image: UIImage(named: "SushiBanner").imageWithRenderingMode(.AlwaysTemplate))
+        image.tintColor = UIColor.whiteColor()
+        image.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        titleLabel = UILabel()
+        titleLabel.textAlignment = .Center
+        titleLabel.backgroundColor = UIColor.clearColor()
+        titleLabel.font = UIFont(name: "Avenir-Light", size: 36.0)
         
         super.init(coder: aDecoder)
     }
@@ -46,26 +59,61 @@ class MenuHeaderView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        self.addSubview(self.icon)
-
+        NSLog("Layout Subviews");
         
         /**
-        *
+        *   Define a standard margin.
         */
         
-        let heightConstraint = NSLayoutConstraint(item: self.icon, attribute: .Width, relatedBy: .Equal, toItem: self.icon, attribute: .Height, multiplier: 1, constant: 0)
-        let widthConstraint = NSLayoutConstraint(item: self.icon, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: CGRectGetHeight(self.frame)*4/5)
-        let centerHorizontalConstraint = NSLayoutConstraint(item: self.icon, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0)
-        let centerVerticalConstraint = NSLayoutConstraint(item: self.icon, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0)
+        let margin: CGFloat = 8.0
         
         /**
-        *
+        *   Constrains for the image
         */
         
-        self.addConstraint(heightConstraint)
-        self.addConstraint(widthConstraint)
-        self.addConstraint(centerHorizontalConstraint)
-        self.addConstraint(centerVerticalConstraint)
+        let imageCenterConstraint = NSLayoutConstraint(item: self.image, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0)
+        let imageTop = NSLayoutConstraint(item: self.image, attribute: .Top, equalToItem: self.titleLabel, attribute: .Bottom, constant: margin)
+        let imageWidthConstraint = NSLayoutConstraint(item: self.image, attribute: .Width, equalToItem: self, attribute: .Width, constant: -80.0)
         
+        /**
+        *   Constraints for the label - uses our class extension.
+        */
+        
+        let labelLeftConstraint = NSLayoutConstraint(item: self.titleLabel, attribute: .Leading, equalToItem: self, attribute: .Left, constant: margin)
+        let labelRightConstraint = NSLayoutConstraint(item: self.titleLabel, attribute: .Trailing, equalToItem: self, attribute: .Right, constant: margin)
+        let labelHeightConstraint = NSLayoutConstraint(item: self.titleLabel, attribute: .Height, equalToItem: self.image, attribute: .Height)
+        let labelTopConstraint = NSLayoutConstraint(item: self.titleLabel, attribute: .Top, equalToItem: self, attribute: .Top, constant: margin)
+        
+        /**
+        *   A constraint for the height of the view.
+        */
+        
+        let imageHeight = CGRectGetHeight(self.image.frame)
+        
+        /**
+        *   Install the views in the hierarchy.
+        */
+        
+        self.addSubview(self.image)
+        self.addSubview(self.titleLabel)
+        
+        /*
+        *   Turn off autoresizing-to-layout-constraint conversions.
+        */
+        
+        for view in self.subviews
+        {
+            view.setTranslatesAutoresizingMaskIntoConstraints(false)
+        }
+        
+        /**
+        *   Install the constraints.
+        */
+        
+        let constraintsToAdd = [labelLeftConstraint, labelRightConstraint, labelHeightConstraint, labelTopConstraint, imageTop, imageCenterConstraint, imageWidthConstraint]
+        
+        for constraint in constraintsToAdd {
+            self.addConstraint(constraint)
+        }
     }
 }
