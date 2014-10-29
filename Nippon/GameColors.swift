4592acf207a1031d572d74492f065c7e896ca31f
Moshe Berman <moshberm@gmail.com>
2014-10-28 20:11:52 -0400

    Added a class for game colors.

---
+++ Nippon/GameColors.swift
@@ -0,0 +1,19 @@
+//
+//  GameColors.swift
+//  Nippon
+//
+//  Created by Moshe on 10/27/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class Colors  {
+    	
+    var redColor : UIColor {
+        get {
+            return  UIColor(red: 0.76, green: 0.14, blue: 0.11, alpha: 1.00)
+        }
+    }
+    
+}
